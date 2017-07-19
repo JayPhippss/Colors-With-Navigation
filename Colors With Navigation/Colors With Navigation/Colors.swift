e69a207e93cbb6775d9d0c0b91f3873889cb7e58
JayPhippss <PhippsJaylin@gmail.com>
2017-07-18 19:27:58 -0500

    Table views with colors text and background colors

---
+++ Colors With Navigation/Colors With Navigation/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  Colors With Navigation
+//
+//  Created by Jaylin Phipps on 7/18/17.
+//  Copyright © 2017 Jaylin Phipps. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
